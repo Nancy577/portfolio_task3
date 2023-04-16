e219e1f55bbb5f5c650fd7c5a0ff6f73b4dab3fd
Nancy khandelwal <85386255+Nancy577@users.noreply.github.com>
2023-04-16 17:21:13 +0530

    Add files via upload

---
+++ src/About.js
@@ -0,0 +1,22 @@
+import React from "react"
+const About = () => {
+    return (
+        <section class="about" id="about">
+        <div class="max-width">
+            <h2 class="title">About me</h2>
+            <div class="about-content">
+                <div class="column left">
+                    <img src="https://th.bing.com/th/id/OIP.Fr0PK_ibSs6WoNTjajkXNAHaE7?pid=ImgDet&rs=1" alt=""/>
+                </div>
+                <div class="column right">
+                    <div class="text">I'm Nancy and I'm a Developer <span class="typing-2"></span></div>. 
+        <p>Recently pursuing masters of computer application with a specialization in full stack development.Completed my Bachelor's degree in Bachelor's of computer application.I have a basic Knowledge in programming languages such as HTML,CSS,javascript,C,SQL. Passionate about web Developer and eager to begin a career in the field.  </p>
+          
+                    <a href="#">Download CV</a>
+                </div>
+            </div>
+        </div>
+    </section>
+    )
+}
+export default About;
---
+++ src/Project.js
@@ -0,0 +1,36 @@
+import React from "react"
+const Project = () => {
+    return (
+        <section class="services" id="services">
+        <div class="max-width">
+            <h2 class="title">My Projects</h2>
+            <div class="serv-content">
+                <div class="card">
+                    <div class="box">
+                        <i class="fas fa-paint-brush"></i>
+                        <div class="text">Web Developmemt and Designing</div>
+                        <p> I have created Basic Banking System dummy website.In this I have used Rosarpay for payment gateway.Languages used HTML,CSS. 
+                        </p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <i class="fas fa-chart-line"></i>
+                        <div class="text">C Language project</div>
+                        <p>In this project I have created a guess the number game and apple game and guess the number game.In this task we have used C and C++ language.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <i class="fas fa-code"></i>
+                        <div class="text">Education Website</div>
+                        <p>In this Project I have Created a Dummy Education Website using Html and CSS.</p>
+                    </div>
+                </div>
+               </div>
+            </div>
+       
+    </section>
+     )
+    }
+    export default Project;
